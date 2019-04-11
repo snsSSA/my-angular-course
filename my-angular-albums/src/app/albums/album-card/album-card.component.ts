a3fa8be54736c916fb1964e2b678a9e51cfae074
snsSSA <sergei.selezneff@gmail.com>
2019-04-11 10:14:25 -0400

    up to pipes

--- my-angular-albums/src/app/albums/album-card/album-card.component.ts
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -15,9 +15,16 @@ export class AlbumCardComponent implements OnInit {
     @Output()
     albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
 
+    newPrice: number;
+
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        if (this.album.on_sale) {
+            // Apply 10% discount
+            this.newPrice = this.album.price - this.album.price * 0.1;
+        }
+    }
 
     showAlbum() {
         this.albumClicked.emit(this.album);
--- my-angular-albums/src/app/albums/album.model.ts
+++ my-angular-albums/src/app/albums/album.model.ts
@@ -1,14 +1,22 @@
 export interface Album {
-  id: number;
- artist: string;
- album_name: string;
- genre: string;
- price: number;
- currency?: string;
-  on_sale: boolean;
-  year: number;
-  release_date: string;
-  recording_location: string;
-  duration: string;
-  url: string;
+    id: number;
+    artist: string;
+    album_name: string;
+    genre: string;
+    price: number;
+    currency?: string;
+    on_sale: boolean;
+    year: number;
+    release_date: string;
+    recording_location: string;
+    duration: string;
+    url: string;
+    tracks: Track[];
+}
+
+export interface Track {
+    id: number;
+    track_number: number;
+    title: string;
+    length: string;
 }
